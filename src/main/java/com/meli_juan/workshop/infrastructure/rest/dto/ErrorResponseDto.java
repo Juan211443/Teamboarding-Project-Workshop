32bf98d1a105ef50c8eb7601ce2f3f176e736d4a
Juan Camilo Amaya Palacios <juan.amayapalacios@mercadolibre.com.co>
2026-07-09 18:34:42 -0500

    refactor: convert class to record

--- src/main/java/com/meli_juan/workshop/infrastructure/rest/dto/ErrorResponseDto.java
+++ src/main/java/com/meli_juan/workshop/infrastructure/rest/dto/ErrorResponseDto.java
@@ -1,17 +1,5 @@
 package com.meli_juan.workshop.infrastructure.rest.dto;
 
-import lombok.Getter;
 import java.time.Instant;
 
-@Getter
-public class ErrorResponseDto {
-    private final String message;
-    private final int status;
-    private final Instant timestamp;
-
-    public ErrorResponseDto(Instant timestamp, int status, String message) {
-        this.timestamp = timestamp;
-        this.status = status;
-        this.message = message;
-    }
-}
+public record ErrorResponseDto(Instant timestamp, int status, String message) {}
