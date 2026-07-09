32896b6230a59f7196cac7d5918b8ce0ee99d487
Juan Camilo Amaya Palacios <juan.amayapalacios@mercadolibre.com.co>
2026-07-09 11:47:46 -0500

    feat: add exeptions to GlobalExceptionHandler

---
+++ src/main/java/com/meli_juan/workshop/infrastructure/rest/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.meli_juan.workshop.infrastructure.rest.exception;
+
+import com.meli_juan.workshop.domain.exception.NegativePriceException;
+import com.meli_juan.workshop.domain.exception.ProductNotFoundException;
+import com.meli_juan.workshop.infrastructure.rest.dto.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.time.Instant;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleNotFound(ProductNotFoundException exception){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponseDto(Instant.now(), 404 , exception.getMessage()));
+    }
+
+    @ExceptionHandler(NegativePriceException.class)
+    public ResponseEntity<ErrorResponseDto> handlerBadRequest(NegativePriceException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponseDto(Instant.now(), 400 , exception.getMessage()));
+    }
+}
