46388df720088105ac880662a15425aa3ec46c30
Juan Camilo Amaya Palacios <140752174+Juan211443@users.noreply.github.com>
2026-07-15 14:16:51 -0500

    hotfix: remove exposed db password and use Spring profiles (#12)
    Co-authored-by: Juan Camilo Amaya Palacios <juan.amayapalacios@mercadolibre.com.co>

--- src/test/java/com/meli_juan/workshop/usecase/ProductUseCaseTest.java
+++ src/test/java/com/meli_juan/workshop/usecase/ProductUseCaseTest.java
@@ -16,7 +16,8 @@ import java.math.BigDecimal;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
